3c845c35acb997b0dfcc317eff968195b866f452
meycry <timchukvadim@gmail.com>
2024-05-13 23:08:53 +0200

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<h1>Welcome to SvelteKit</h1>
+<p>
+  Visit <a href="/coffee">the coffee</a> page
+</p>
---
+++ src/routes/coffee/+page.svelte
@@ -0,0 +1,5 @@
+<h1>The page lorem</h1>
+<p>
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci amet, deleniti dolores eius, enim expedita ipsam
+  iste labore modi molestiae suscipit voluptates. Aperiam error ex vero? Ad, blanditiis, odio! Amet.
+</p>
